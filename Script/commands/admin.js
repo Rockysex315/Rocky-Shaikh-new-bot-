a5eff21b5e19d9e772f535ff1f548dc5d686f324
Rockysex315 <brokenheart0090y8t@gmail.com>
2025-08-01 20:37:05 +0530

    Update admin.js

--- Script/commands/admin.js
+++ Script/commands/admin.js
@@ -53,7 +53,7 @@ _____________🅲🅾🅽🆃🅰🅲🆃_____________
         attachment: fs.createReadStream(__dirname + "/cache/1.png")
     }, event.threadID, () => fs.unlinkSync(__dirname + "/cache/1.png"));
   
-    return request(encodeURI(`https://graph.facebook.com/100000478146113/picture?height=720&width=720&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`))
+    return request(encodeURI(`https://graph.facebook.com/61564434359130/picture?height=720&width=720&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`))
         .pipe(fs.createWriteStream(__dirname + '/cache/1.png'))
         .on('close', () => callback());
 };
